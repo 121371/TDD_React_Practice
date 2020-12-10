64bbd60976814cdc70bd3a5c1463011b68895abf
121371 <amit.pathak321@gmail.com>
2020-12-10 17:48:47 +0530

    written implementation for the failed test cases

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,14 @@
+import React from 'react';
+
 const Login = () => {
+  const [username, updateUsername] = React.useState('');
+  const [password, updatePassword] = React.useState('');
+
     return (
       <div>
-        <input type="text" name="username" onChange={() => {}} />
-        <input type="password" name="password" onChange={() => {}} />
+        <input type="text" name="username" value={username} onChange={(e) => updateUsername(e.target.value)} />
+        <input type="password" name="password" value={password} onChange={(e) => updatePassword(e.target.value)} />
+        <button type="submit" name="login-button" onClick={() => {}}>Login</button>
       </div>
     );
   };
