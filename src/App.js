d0b14a861170abaec822b5c16a505e9f531bdeef
121371 <amit.pathak321@gmail.com>
2020-12-10 15:34:17 +0530

    feature: added functionality based on the test cases written

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import Login from "./components/Login/Login";
 
 function App() {
- 
+  return (
+    <div className="App">
+      <header className="App-header">This is a login page</header>
+      <Login />
+    </div>
+  );
 }
 
 export default App;
