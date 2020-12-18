a2849deab330ca3023e94e9a7f333b4e43a53c04
121371 <amit.pathak321@gmail.com>
2020-12-19 00:40:34 +0530

    test: test cases for register component

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register: React.FC = () => {
-  return <div>I am in regsiter</div>;
+    const [fName, setFirstName] = useState<string | undefined>(undefined);
+  return (
+    <div className="register">
+        I am in regsiter
+        <input type="text" name="firstName" value={fName} className="first-name" onChange={(e) => setFirstName(e.target.value)}/>
+    </div>
+  )
 };
 
 export default Register;
